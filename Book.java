f45f56eb7e06f599bca6d98e1d14bcd829596f1f
ShinaOdukoya <shinaodukoya2@gmail.com>
2019-05-14 16:07:20 +0100

    Second push

--- Book.java
+++ Book.java
@@ -6,7 +6,17 @@ public class Book {
        private boolean isHardCover;
        private int Pages;
        private String title;
-        
-        
-    
+
+       public void setAuthor (String author){
+           this.author = author;
+       }
+       public getAuthor (){
+           return author;
+       }
+       public void setIsHardCover(boolean isHardCover){
+           this.isHardCover = isHardCover;
+       }
+       public boolean getIsHardCover (){
+           return isHardCover;
+       }
 }
